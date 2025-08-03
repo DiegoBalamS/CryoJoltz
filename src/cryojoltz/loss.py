670082085ba5ee6d4361163ef2e01d81957ba02b
Diego Balam Sanchez Espinosa <152104666+DiegoBalamS@users.noreply.github.com>
2025-08-03 19:23:40 -0400

    Update loss.py

--- src/cryojoltz/loss.py
+++ src/cryojoltz/loss.py
@@ -1,1 +1,16 @@
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+import numpy as np
+from cryojoltz.images import simulate_image_from_atoms
+from cryojoltz.discrepancy import image_distance
 
+def loss_fn(model, features, key, target_images):
+    pred = model(features, key=key, sample_structure=True)
+    coords = pred["sample_atom_coords"][0]
+    atom_types_onehot = features["ref_element"][0]
+    identities = jnp.argmax(atom_types_onehot, axis=-1)
+    generated = simulate_image_from_atoms(coords,identities)
+    return image_distance(generated, target_images)
+
+grad_fn = eqx.filter_value_and_grad(loss_fn)
